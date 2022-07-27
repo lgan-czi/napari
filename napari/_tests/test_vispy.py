2a209aaf7a7e555930e3dcf04c3db2e14f21318c
lgan-czi <lgan@CZIMACOS4099.local>
2022-07-27 12:09:39 -0700

    added test

---
+++ napari/_tests/test_vispy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from napari._vispy.thumbnail import VispyThumbnail
+from napari.layers import Image
+
+
+def test_image(make_napari_viewer):
+    data = np.random.randint(255, size=(32, 32, 4))
+    layer = Image(data)
+    thumbnail = VispyThumbnail(layer)
+
+    thumbnail_image = thumbnail.get_image()
+
+    np.testing.assert_array_equal(data, thumbnail_image)
