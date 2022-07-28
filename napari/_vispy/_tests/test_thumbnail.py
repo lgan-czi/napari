001b4cdb6c1b1c1466f68821b1a8c38f877556a5
Andy Sweet <andrew.d.sweet@gmail.com>
2022-07-27 17:28:14 -0700

    Add a broken test

---
+++ napari/_vispy/_tests/test_thumbnail.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from napari._vispy.thumbnail import VispyThumbnail
+from napari.layers import Image
+
+
+def test_image():
+    # np.random.seed(0)
+    # data = np.random.randint(255, size=(32, 32, 4), dtype=np.uint8)
+    data = 255 * np.ones((32, 32, 4), dtype=np.uint8)
+    data[:, :, 3] = 255
+    layer = Image(data, rgb=True)
+    thumbnail = VispyThumbnail(layer)
+
+    thumbnail_image = thumbnail.get_image()
+
+    plt.imshow(thumbnail_image)
+    plt.show()
+
+    np.testing.assert_array_equal(data, thumbnail_image)
